81c42c8eecc3255f2aead1acab86ef7a4d378981
Brice Leroy <bbrriiccee@gmail.com>
2016-04-30 19:52:17 -0700

    Rename Domain to ActionHandler

--- index.js
+++ index.js
@@ -2,15 +2,5 @@
 
 const DAS = module.exports = {};
 
-DAS.Domain = require('./Domain');
-
-// const SocketServer = require('./SocketServer');
-
-// DWS.createSocketServer = function(options, fn){
-//   return new SocketServer(options, fn);
-// }
-
-// DWS.createHttpServer = function(options, fn){
-//   return new HttpServer(options, fn);
-// }
+DAS.ActionHandler = require('./ActionHandler');
 
